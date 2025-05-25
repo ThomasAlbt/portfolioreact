c855c62958cd913792dc72f9e9f55632a34ee7aa
ThomasAlbt <thomas.albert.jv@gmail.com>
2025-05-25 23:45:22 +0100

    modified the text in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <>
             <div className="paragraphe">
-                <p>Je m'appelle Thomas Albert, 24 ans, passionné par le développement web et les défis techniques. En formation de Développeur Web Fullstack à la 3W Academy, je cherche à appliquer mes compétences en HTML, CSS, PHP, JavaScript, MySQL, React et Node.js.</p>
+                <p>Je m'appelle Thomas Albert, 24 ans, passionné par le développement web et les défis techniques. En formation de Développeur Web Fullstack à la 3W Academy, je cherche à appliquer mes compétences.</p>
                 <p>Mon parcours inclut une 1ère année de licence de math-info et une expérience en restauration, m'enseignant endurance et gestion du stress. En dehors du travail, je suis fan de Formule 1 et de jeux à énigmes, renforçant ma capacité à résoudre des problèmes et à travailler en équipe.</p>
             </div>
             <h2>A propos</h2>
