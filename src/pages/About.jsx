dded249753ef524e304747fed47a3f4ab9605bf8
ThomasAlbt <thomas.albert.jv@gmail.com>
2025-07-22 22:45:08 +0100

    changed some text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
     return (
         <>
             <div className="paragraphe">
-                <p>Je m'appelle Thomas Albert, j'ai 24 ans, et je suis passionné par le développement web. Après trois mois de formation intensive à la 3W Academy, j’ai continué à me former en autodidacte. J’ai notamment validé deux certifications FreeCodeCamp — <strong>Responsive Web Design</strong> et <strong>JavaScript Algorithms and Data Structures</strong> — tout en réalisant des projets concrets en React et Node.js. Je suis aujourd’hui prêt à rebondir et à m’investir pleinement dans une opportunité professionnelle ou formatrice.</p>
-                <p>Avant ça, j’ai travaillé un an et demi en restauration, une expérience exigeante qui m’a appris la rigueur, la persévérance et la gestion du stress. En dehors du code, je suis passionné par la Formule 1 et les jeux d’énigmes, deux univers qui nourrissent mon goût pour la logique, l’analyse et les défis techniques. Je suis déterminé, curieux, et toujours partant pour apprendre de nouvelles choses.</p>
+                <p>Je m’appelle Thomas, 24 ans, développeur web junior passionné. Formé à la 3W Academy, j’ai validé deux certifications FreeCodeCamp et réalisé plusieurs projets en React. Je recherche une nouvelle alternance pour mettre à profit mes compétences et continuer à apprendre.</p>
+                <p>Fan de Formule 1 (même si Ferrari me fait parfois tourner en bourrique), je suis aussi un joueur assidu de Teamfight Tactics et Final Fantasy XIV, où stratégie et adaptation sont indispensables — un bon entraînement pour coder avec logique et créativité !</p>
             </div>
             <h2>A propos</h2>
         </>
